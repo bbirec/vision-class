c61bc15b0ba6b9ce92034a01b6473a2e1512f19b
Heehong Moon <bbirec@gmail.com>
2012-03-28 21:20:39 +0900

    Cleaning the code.

--- hw1/hw1.lisp
+++ hw1/hw1.lisp
@@ -10,24 +10,56 @@
 (defvar *img1-half* nil)
 (defvar *img2-half* nil)
 
+;;; PNG read and write
+
 (defun read-images (filenames)
   (loop for f in filenames collect
        (read-png-file (concatenate 'string *path* f))))
-    
 
-(defun main ()
-  (destructuring-bind (mixed img1 img2 img1-half img2-half)
-      (read-images '("car_eagle.png" 
-		     "car_filtered.png" 
-		     "eagle_filtered.png"
-		     "car_filtered_half2.png"
-		     "eagle_filtered_half2.png"))
-    (setf *mixed* mixed
-	  *img1* img1
-	  *img2* img2
-	  *img1-half* img1-half
-	  *img2-half* img2-half)))
 
+(defun write-png-file (filename matrix)
+  (let* ((w (cols matrix))
+	 (h (rows matrix))
+	 (png (make-instance 'zpng:png 
+			     :color-type :grayscale
+			     :width w
+			     :height h))
+	 (image (zpng:data-array png)))
+    (labels ((clamp (x min max)
+	       (if (< x min) min
+		   (if (> x max) max x))))
+
+      (loop for j from 0 below h do
+	   (loop for i from 0 below w do
+		(setf (aref image j i 0) 
+		      (round (clamp (mref matrix j i) 0.0 255.0)))))
+      (zpng:write-png png (concatenate 'string *path* filename)))))
+
+;; Resize image
+
+
+(defun sub-img (img x y w h)
+  (let* ((data (image-data img))
+	 (result (make-array (list w h))))
+    (loop for j from 0 below h do
+	 (loop for i from 0 below w do
+	      (setf (aref result i j) (aref data (+ i x) (+ j y)))))
+    result))
+
+
+(defun resize-img (filename img x y w h)
+  "Resize IMG with the bound box and save it."
+  (let ((data (sub-img img x y w h))
+	(mat (dnew 0 h w)))
+    (loop for j from 0 below h do
+	 (loop for i from 0 below w do
+	      (setf (mref mat j i) (aref data i j))))
+    (write-png-file filename mat)))
+
+	
+
+
+;;; Matrix reshape
 
 (defun arr->mat (arr)
   ;; Array check routine.
@@ -56,55 +88,7 @@
       (dotimes (c w)
 	(setf (mref mat r c) (mref cmat (+ (* r w) c) 0))))))
 
-(defun find-source (x y)
-  (let ((data (image-data *mixed*))
-	(values nil))
-    (loop for j from -1 to 1 do
-	 (loop for i from -1 to 1 do
-	      (setf values (cons (aref data (+ x i) (+ y j)) values))))
-    (reverse values)))
-
-(defun filtered-value (x y)
-  (let ((i1 (image-data *img1*))
-	(i2 (image-data *img2*)))
-    (/ (+ (aref i1 x y) (aref i2 x y)) 2.0)))
-
-
-(defun solve (points)
-  (let* ((source (arr->mat 
-		  (loop for (x y) in points collect
-		       (find-source x y))))
-	 (target (arr->mat 
-		  (loop for (x y) in points collect
-		       (list (filtered-value x y))))))
-
-    (if (= (rows source) (cols source))
-	;; Inverse matrix
-	(progn 
-	  (format t "Multiplying inverse matrix~%")
-	  (m* (minv source) target))
-	
-	;; Least square solution
-	(progn 
-	  (format t "Least square solution~%")
-	  (let ((source-tp (mtp source)))
-	    (m* (m* (minv (m* source-tp source)) source-tp) target))))))
-
-(defvar *h* nil)
-
-(defun least-square-solve ()
-  (let* ((w (width *mixed*))
-	 (h (height *mixed*))
-	 (points
-	  (loop for j from 1 below (- h 1) append
-	       (loop for i from 1 below (- w 1) collect
-		    (list i j)))))
-    (setf *h* (solve points))))
-	      
-
-
-
-;; Reconstruction of original image
+;;; Pixel position operation
 
 (defmacro do-pixels (w h padding (x y) &body body)
   "Iterate all of pixels with binding variables X and Y."
@@ -120,18 +104,67 @@
        (loop for i from -1 to 1 collect
 	    (list (+ x i) (+ y j)))))
 
-;; [filter or zero][Original image vector]=[I1]
+;;; Linear eq
+
+(defun solve-eq (A b)
+  "Solving linear equation Ax=b"
+  (if (= (rows A) (cols A))
+      (m* (minv A) b)
+      (let ((A-tp (mtp A)))
+	(m* (m* (minv (m* A-tp A)) A-tp) b))))
+
+
+;; Finding filter
+;; => [Average image][h]=[I1*h/2 + I2*h/2]
+
+(defun find-source (x y)
+  (let ((data (image-data *mixed*))
+	(values nil))
+    (loop for j from -1 to 1 do
+	 (loop for i from -1 to 1 do
+	      (setf values (cons (aref data (+ x i) (+ y j)) values))))
+    (reverse values)))
+
+
+(defun filtered-value (x y)
+  (let ((i1 (image-data *img1*))
+	(i2 (image-data *img2*)))
+    (/ (+ (aref i1 x y) (aref i2 x y)) 2.0)))
+
+	      
+(defun find-filter ()
+  (let* ((w (width *mixed*))
+	 (h (height *mixed*))
+	 (points
+	  (loop for j from 1 below (- h 1) append
+	       (loop for i from 1 below (- w 1) collect
+		    (list i j))))
+	 (source (arr->mat 
+		    (loop for (x y) in points collect
+			 (find-source x y))))
+	 (target (arr->mat 
+		  (loop for (x y) in points collect
+		       (list (filtered-value x y))))))
+    ;; Least square solution
+    (solve-eq source target)))
+
+
+
+
+;; Reconstruction of original image
+;; => [filter or zero][Original image vector]=[I1]
 
 (defun reconstruction (img filter)
   (let* ((w (width img))
 	 (h (height img))
+	 (data (image-data img))
 	 (w-2 (- w 2))
 	 (h-2 (- h 2))
 	 (size (* w h))
 	 (size-2 (* w-2 h-2))
 	 (src (dnew 0 size-2 size))
-	 (dst (dnew 0 size-2))
-	 (data (image-data img)))
+	 (dst (dnew 0 size-2)))
+
     (do-pixels w h 1 (x y)
       (let ((row (+ (* (- y 1) w-2) (- x 1))))
 	;; Fill src      
@@ -149,38 +182,37 @@
 	   (q (m* src-tp-inv src-tp))
 	   (solution (m* q dst)))
 
-      #-nil
       (write-png-file "verification.png" (cmat->mat (m* src solution) w-2 h-2))
+      (write-png-file "original.png" (cmat->mat dst w-2 h-2))
       
       (list solution w h))))
 	   
 
-(defun clamp (x min max)
-  (if (< x min) min
-      (if (> x max) max x)))
       
 	  
-(defun write-png-file (filename matrix)
-  (let* ((w (cols matrix))
-	 (h (rows matrix))
-	 (png (make-instance 'zpng:png 
-			     :color-type :grayscale
-			     :width w
-			     :height h))
-	 (image (zpng:data-array png)))
-    (loop for j from 0 below h do
-	 (loop for i from 0 below w do
-	      (setf (aref image j i 0) 
-		    (round (clamp (mref matrix j i) 0.0 255.0)))))
-    (zpng:write-png png (concatenate 'string *path* filename))))
 
-
+(defvar *h* nil)
 (defvar *original-1* nil)
 (defvar *original-2* nil)
 
+
+(defun main ()
+  (destructuring-bind (mixed img1 img2 img1-half img2-half)
+      (read-images '("car_eagle.png" 
+		     "car_filtered.png" 
+		     "eagle_filtered.png"
+		     "car_filtered_part.png"
+		     "eagle_filtered_part.png"))
+    (setf *mixed* mixed
+	  *img1* img1
+	  *img2* img2
+	  *img1-half* img1-half
+	  *img2-half* img2-half)))
+
+
 	   
 (defun result ()
-  (let* ((h (least-square-solve))
+  (let* ((h (find-filter))
 	 (a (cmat->arr h)))
     (format t "Question a : ~A" a)
 
