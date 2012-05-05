1fced81d02c0ea8ab067f8371f8e955034347dde
Heehong Moon <bbirec@gmail.com>
2012-05-06 01:58:54 +0900

    Adding k-means clustering.

--- hw3/hw3.lisp
+++ hw3/hw3.lisp
@@ -99,8 +99,92 @@
 	
 
 
+;; Image clustering
 
+(defun pick-k-random (k start end)
+  "Pick k random values between start and end"
+  (assert (>= (- end start) k))
+  (let ((values nil))
+    (loop for v = (- (random end) start) 
+       until (>= (length values) k)
+       do (pushnew v values))
+    values))
+
+(defun dist-2 (v1 v2)
+  (assert (= (length v1) (length v2)))
+  (reduce #'+ (mapcar #'(lambda (x) (* x x)) (mapcar #'- v1 v2))))
+
+(defun sum-list (v1 v2)
+  (mapcar #'+ v1 v2))
+
+(defun find-cluster-idx (k-means d)
+  (let ((min-dist (dist-2 (car k-means) d))
+	(idx 0))
+    (loop for k in (cdr k-means)
+       for i from 1 do
+	 (let ((dist (dist-2 k d)))
+	   (when (< dist min-dist)
+	     (setf min-dist dist)
+	     (setf idx i))))
+    (values idx min-dist)))
+  
+(defun p= (p1 p2)
+  (and (= (length p1) (length p2))
+       (every #'zerop (mapcar #'- p1 p2))))
+  
+(defun cluster= (c1 c2)
+  (and (= (length c1) (length c2))
+       (loop for p in c1 always
+	    (member p c2 :test #'p=))))
+       
+
+(defun k-means (k data)
+  "Perform k-means clustering algorithm"
+  
+  (let ((k-means nil)
+	(clusters (make-array k :initial-element nil)))
+
+    ;; Select k-means randomly
+    (let ((init-idx (pick-k-random k 0 (length data))))
+      (loop for idx in init-idx do
+	   (push (nth idx data) k-means)))
+
+    ;; Iterate until convergence of k-means
+    (loop for i below 100 do
+	 ;; Assignment step
+	 (let ((new-clusters (make-array k :initial-element nil)))
+	   (loop for d in data do
+		(let ((idx (find-cluster-idx k-means d)))
+		  (push d (aref new-clusters idx))))
+
+	   (format t "New cluster: ~A~%" new-clusters)
+	   (format t "Last cluster: ~A~%" clusters)
+	   
+	   ;; Terminate if the clusters are same with the last one
+	   (if (loop for j below k always
+			(cluster= (aref clusters j) (aref new-clusters j)))
+	       (progn
+		 (format t "End of cluster~%")
+		 (return-from nil))
+	       (progn 
+		 ;; Set new clusters
+		 (setf clusters new-clusters)
+
+		 ;; Update step
+		 (setf k-means 
+		       (loop for j below k collect
+			    (let* ((cluster (aref clusters j))
+				   (len (length cluster))
+				   (s (reduce #'sum-list cluster)))
+			      (mapcar #'(lambda (x) (/ x len)) s))))))))
+    (values clusters k-means)))
+		
+		
+		   
+		   
+	      
       
+       
 
 
 
