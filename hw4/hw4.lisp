8bf88af0c349eab1c109a19ae4906f831ede21fa
Heehong Moon <bbirec@gmail.com>
2012-06-02 00:07:45 +0900

    Showing the image and calculate the result.

--- hw4/hw4.lisp
+++ hw4/hw4.lisp
@@ -37,11 +37,15 @@
 		 (+ (aref h idx) 1))))
     (coerce h 'list)))
 	 
-(defun histogram-rect (image-pixel-arr image-width x y w h)
-  (histogram 
-   (loop for j from y below (+ y h) append
-	(loop for i from x below (+ x w) collect
-	     (aref image-pixel-arr (+ (* image-width j) i))))))
+(defun histogram-rect (image-pixel-arr image-width rect)
+  (let ((x (caar rect))
+	(y (cadar rect))
+	(w (- (caadr rect) (caar rect)))
+	(h (- (cadadr rect) (cadar rect))))
+    (histogram 
+     (loop for j from y below (+ y h) append
+	  (loop for i from x below (+ x w) collect
+	       (aref image-pixel-arr (+ (* image-width j) i)))))))
 
 
 
@@ -73,7 +77,120 @@
 	   (total (reduce #'+ samples)))
       (mapcar #'(lambda (x) (/ x total))
 	      samples))))
-	   
+
+(defun gaussian-sample (size k sigma)
+  (mapcar #'(lambda (x) (round (* k x)))
+	  (gaussian-filter (+ (* size 2) 1) sigma)))
+
+(defparameter *gaussian-weight* '(1 3 7 12 18 20 18 12 7 3 1))
+
+(defun random-from-to (from to)
+  (if (= from to) 
+      from
+      (+ (random (- to from)) from)))
+
+(defun gaussian-random-bound (bound)
+  (let ((values (loop for i from (- (/ bound 2)) to (/ bound 2)
+		   by (/ bound (length *gaussian-weight*)) collect i)))
+
+    (loop for i below (- (length values) 1) append
+	 (let ((i1 (floor (nth i values)))
+	       (i2 (floor (nth (+ i 1) values))))
+
+	   (loop for j below (nth i *gaussian-weight*) collect
+		(random-from-to i1 i2))))))
+
+(defun seqrnd (seq)
+  "Randomize the elements of a sequence. Destructive on SEQ."
+  (sort seq #'> :key (lambda (x) (random 1.0))))
+		      
+(defun random-rect-with-gaussian (rect diff)
+  "Generating random K rects"
+  (let* ((ul (car rect))
+	 (lr (cadr rect))
+	 (bounds1 (seqrnd (gaussian-random-bound diff)))
+	 (bounds2 (seqrnd (gaussian-random-bound diff)))
+	 (bounds3 (seqrnd (gaussian-random-bound diff)))
+	 (bounds4 (seqrnd (gaussian-random-bound diff))))
+    (mapcar #'list 
+	    (loop for dx in bounds1
+	       for dy in bounds2 collect
+	       (list (+ (car ul) dx) (+ (cadr ul) dy)))
+	    (loop for dx in bounds3
+	       for dy in bounds4 collect
+	       (list (+ (car lr) dx) (+ (cadr lr) dy))))))
+    
+(defvar *ref-rect* nil)
+(defvar *cur-rect* nil)
+
+(defvar *rects* nil)
+
+(defvar *random-rects* nil)
+
+(defvar *cur-position* nil)
+
+
+(defvar *image-idx* 0)
+(defparameter *image-paths* nil)
+
+
+(defvar *image* nil)
+(defvar *image-pixels* nil)
+
+(defun load-image (idx)
+  (setf *image-idx* idx
+	*image* (sdl-image:load-image (nth idx *image-paths*))
+	*image-pixels* (surface->pixel-arr *image*)))
+
+(defun draw-rect (r color)
+  (sdl:draw-rectangle-* (caar r)
+			(cadar r)
+			(- (caadr r) (caar r))
+			(- (cadadr r) (cadar r))
+			:color color))
+
+(defun draw ()
+  (if *image*
+    (sdl:draw-surface-at *image* (sdl:point :x 0 :y 0)))
+
+  ;; Draw random rects
+  (loop for r in *random-rects* do
+       (draw-rect r sdl:*yellow*))
+
+  ;; Draw current rect
+  (when *cur-rect* 
+    (draw-rect *cur-rect* sdl:*white*)))
+  
+
+(defun load-next-image ()
+  (setf *image-idx* (+ 1 *image-idx*))
+  (format t "Load ~A image.~%" *image-idx*)
+  (load-image *image-idx*)
+
+  ;; Generate the next rect
+  (setf *random-rects* (random-rect-with-gaussian *cur-rect* 50))
+  
+  ;; set rect
+  (push *cur-rect* *rects*)
+
+  ;; Find best rect
+  (let ((hs (loop for r in *random-rects* collect
+		 (distance-histogram *ref-histogram*
+				     (histogram-rect *image-pixels* 720 r)))))
+    (setf *cur-rect* (nth (position (reduce #'max hs) hs) *random-rects*))))
+    
+
+(defun init ()
+  (setf *image-idx* 0
+	*image* nil
+	*image-pixels* nil
+	*ref-rect* nil
+	*ref-histogram* nil
+	*cur-rect* nil
+	*cur-position* nil
+	*rects* nil
+	*random-rects* nil))
+ 
 
 
 (defun main ()
@@ -87,13 +204,17 @@
     ;; loading of these images.
     (sdl-image:init-image :jpg :png :tif)
 
-    (let* ((image 
-	    (sdl-image:load-image (merge-pathnames "test.pgm" *bmp-path*)))
-	   (pixels (surface->pixel-arr image)))
+    ;; Load image pathnames
+    (let ((images (cl-fad:list-directory 
+		  "/Users/bbirec/Dropbox/Classes/vision/hw4/DudekSeq/")))
+      (setf *image-paths* images
+	    *image-idx* 0
+	    *image* nil
+	    *image-pixels* nil))
 
-      (format t "Pixel: ~A" (array-dimensions pixels))
-      (sdl:draw-surface-at image (sdl:point :x 0 :y 0)))
-      
+    (load-image 0)
+
+
     (sdl:update-display)
       
     (sdl:with-events ()
@@ -104,5 +225,34 @@
 		   t)
       (:key-down-event (:key key)
 		       (if (sdl:key= key :SDL-KEY-ESCAPE)
-			   (sdl:push-quit-event)))
+			   (sdl:push-quit-event))
+		       (if (sdl:key= key :SDL-KEY-SPACE)
+			   ;; Load next image
+			   (load-next-image)))
+
+      (:mouse-button-down-event (:x x :y y)
+				(when (null *ref-rect*)
+				  (format t "X:~A Y:~A~%" x y)
+				  (setf *cur-position* (list x y))
+				  (sdl:update-display)))
+      (:mouse-button-up-event (:x x :y y)
+			      (when (null *ref-rect*)
+				(format t "X:~A Y:~A~%" x y)
+
+				(let* ((rect (list *cur-position* (list x y)))
+				       (h (histogram-rect *image-pixels*
+							  720
+							  rect)))
+				  ;; Set reference rect
+				  (setf *ref-rect* rect
+					*ref-histogram* h
+					*cur-rect* rect))
+
+				(sdl:update-display)))
+
+      (:idle ()
+	     (sdl:clear-display sdl:*black*)
+	     (draw)
+	     (sdl:update-display))
+
       (:video-expose-event () (sdl:update-display)))))
